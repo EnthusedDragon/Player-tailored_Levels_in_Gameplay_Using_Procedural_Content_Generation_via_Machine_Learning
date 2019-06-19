37298b9b4772510d52714e4d5230a8fc78d72448
Enthused Dragon <jamesj1992m.gm@gmail.com>
2019-06-19 13:53:40 +0200

    PCG Maze generator created.

---
+++ PcgPerfectMazeGenerator/Scripts/TopDownCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    bool setup = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!setup)
+            Setup();
+    }
+
+    void Setup()
+    {
+        var parent = GameObject.Find("Maze");
+
+        // First find a center for your bounds.
+        Vector3 center = Vector3.zero;
+
+        foreach (Transform child in parent.transform)
+        {
+            center += child.GetComponent<Renderer>().bounds.center;
+        }
+        center /= parent.transform.childCount; //center is average center of children
+
+        //Now you have a center, calculate the bounds by creating a zero sized 'Bounds', 
+        Bounds bounds = new Bounds(center, Vector3.zero);
+
+        foreach (Transform child in parent.transform)
+        {
+            bounds.Encapsulate(child.GetComponent<Renderer>().bounds);
+        }
+
+        var camHeight = Mathf.Max(bounds.size.x, bounds.size.z);
+        
+        transform.position = new Vector3((bounds.size.x / 2)-2.5f, camHeight * 1.5f, (bounds.size.z / 2) - 2.5f);
+
+        setup = true;
+    }
+}
---
+++ PcgPerfectMazeGenerator/Scripts/UnityCustomEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Scripts
+{
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(MazeLoader))]
+    public class RandomScript_Editor : Editor
+    {
+        //public override void OnInspectorGUI()
+        //{
+        //    DrawDefaultInspector(); // for other non-HideInInspector fields
+
+        //    MazeLoader script = (MazeLoader)target;
+            
+        //    if (script.randomSeed) // if bool is true, show other fields
+        //    {
+        //        script.seed = EditorGUILayout.TextField("Seed", script.seed);
+        //    }
+        //}
+    }
+#endif
+}
